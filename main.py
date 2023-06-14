879c4b3e63af9f4b3a593bff0707b0f1ce15aa3d
Admin <bakuron1996@gmail.com>
2023-06-14 08:42:48 +0700

    commit 1

---
+++ main.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Chẩn đoán bệnh viêm quanh cuống")
+root.resizable(height=False,width=False)
+root.minsize(height=450,width=600)
+
+frame = Frame(root)
+frame.pack(side=BOTTOM,padx=15,pady=15)
+
+
+def makecenter(root):
+    root.update_idletasks()
+    width = root.winfo_width()
+    height = root.winfo_height()
+    x = (root.winfo_screenwidth() // 2) - (width // 2)
+    y = (root.winfo_screenheight() // 2) - (height // 2)
+    root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+
+def selectpic():
+    filename = filedialog.askopenfilename(initialdir="E:\Study\TTM\Viêm quanh cuống goc",title="Select Image",filetypes=(("JPG File","*.jpg"),("PNG File","*.png")))
+    img = Image.open(filename)
+    img.thumbnail((200,200), Image.LANCZOS)
+    img = ImageTk.PhotoImage(img)
+    lbl_show_pic.configure(image=img)
+    lbl_show_pic.image = img
+
+
+lbl_show_pic = Label(root)
+lbl_show_pic.pack()
+
+lbl_show_res_pic = Label(root)
+lbl_show_res_pic.pack()
+
+btn_browse = Button(frame, font=('verdana',16), text='Select Image', bg='grey', command=selectpic,width=11)
+btn_browse.pack(side=tk.LEFT,padx=30)
+
+btn_run = Button(frame, font=('verdana',16), text='Run', bg='grey', command=lambda :exit(),width=11)
+btn_run.pack(side=tk.LEFT,padx=30)
+
+makecenter(root)
+root.mainloop()
